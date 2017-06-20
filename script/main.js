1fd3549494c2f24ee8eb37b355900fa2e5eae6a7
Sean Gilmore <sgilmore@michael-sean.com>
2017-06-20 11:58:15 -0400

    created game logic

--- script/main.js
+++ script/main.js
@@ -1,7 +1,11 @@
-// console.log("Up and running!");
-var cardOne = "king";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "queen";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+cardsInPlay.push(cardOne,cardTwo);
+console.log("User Flipped " + cardsInPlay);
+if (cardsInPlay.length === 2 && cardOne === cardTwo) {
+	alert("You found a match.")
+} else {
+	alert("Sorry, try again.")
+};
